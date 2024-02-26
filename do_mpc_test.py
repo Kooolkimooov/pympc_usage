37362b44e484b58bab3f7df76c583a6cba82fda4
martin filliung <martin-filliung@etud.univ-tln.fr>
2024-02-26 17:49:23 +0100

    works

--- do_mpc_test.py
+++ do_mpc_test.py
@@ -1,6 +1,13 @@
-import casadi
+import importlib.util
 import numpy as np
+import sys
 from casadi import *
+
+# Import do_mpc package:
+import do_mpc
+
+model_type = 'continuous'  # either 'discrete' or 'continuous'
+model = do_mpc.model.Model( model_type )
 
 bluerov_configuration = {
 		"mass"                     : 11.5,
@@ -11,151 +18,162 @@ bluerov_configuration = {
 		"hydrodynamic_coefficients": np.array(
 				[ 4.03, 6.22, 5.18, 0.07, 0.07, 0.07, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
 				),
-		"robot_max_actuation"      : np.array( [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ] ),
-		"robot_max_actuation_ramp" : np.array( [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 ] )
+		"robot_max_actuation"      : np.array( [ 100, 100, 100, 100, 100, 100 ] ),
+		"robot_max_actuation_ramp" : np.array( [ 100, 100, 100, 100, 100, 100 ] )
 		}
 
-
-def compute_IAb( state: SX, robot_configuration = None ) -> tuple:
-
-	global bluerov_configuration
+def get_state_matrixes( state, robot_configuration = None ):
 	if robot_configuration is None:
 		robot_configuration = bluerov_configuration
-
 	mass = robot_configuration[ "mass" ]
 	inertial_coefficients = robot_configuration[ "inertial_coefficients" ]
 	center_of_mass = robot_configuration[ "center_of_mass" ]
-	I = np.eye( 12 )
-	for i in range( 6, 9 ):
+	I = np.eye( 6 )
+	for i in range( 3 ):
 		I[ i, i ] = mass
-		I[ i + 3, i + 3 ] = inertial_coefficients[ i - 6 ]
-	I[ 6, 10 ] = mass * center_of_mass[ 2 ]
-	I[ 6, 11 ] = - mass * center_of_mass[ 1 ]
-	I[ 7, 9 ] = - mass * center_of_mass[ 2 ]
-	I[ 7, 11 ] = mass * center_of_mass[ 0 ]
-	I[ 8, 9 ] = mass * center_of_mass[ 1 ]
-	I[ 8, 10 ] = - mass * center_of_mass[ 0 ]
-	I[ 10, 6 ] = mass * center_of_mass[ 2 ]
-	I[ 11, 6 ] = - mass * center_of_mass[ 1 ]
-	I[ 9, 7 ] = - mass * center_of_mass[ 2 ]
-	I[ 11, 7 ] = mass * center_of_mass[ 0 ]
-	I[ 9, 8 ] = mass * center_of_mass[ 1 ]
-	I[ 10, 8 ] = - mass * center_of_mass[ 0 ]
-	I[ 9, 10 ] = - inertial_coefficients[ 3 ]
-	I[ 9, 11 ] = - inertial_coefficients[ 4 ]
-	I[ 10, 11 ] = - inertial_coefficients[ 5 ]
-	I[ 10, 9 ] = - inertial_coefficients[ 3 ]
-	I[ 11, 9 ] = - inertial_coefficients[ 4 ]
-	I[ 11, 10 ] = - inertial_coefficients[ 5 ]
+		I[ i + 3, i + 3 ] = inertial_coefficients[ i ]
+	I[ 0, 4 ] = mass * center_of_mass[ 2 ]
+	I[ 0, 5 ] = - mass * center_of_mass[ 1 ]
+	I[ 1, 3 ] = - mass * center_of_mass[ 2 ]
+	I[ 1, 5 ] = mass * center_of_mass[ 0 ]
+	I[ 2, 3 ] = mass * center_of_mass[ 1 ]
+	I[ 2, 4 ] = - mass * center_of_mass[ 0 ]
+	I[ 4, 0 ] = mass * center_of_mass[ 2 ]
+	I[ 5, 0 ] = - mass * center_of_mass[ 1 ]
+	I[ 3, 1 ] = - mass * center_of_mass[ 2 ]
+	I[ 5, 1 ] = mass * center_of_mass[ 0 ]
+	I[ 3, 2 ] = mass * center_of_mass[ 1 ]
+	I[ 4, 2 ] = - mass * center_of_mass[ 0 ]
+	I[ 3, 4 ] = - inertial_coefficients[ 3 ]
+	I[ 3, 5 ] = - inertial_coefficients[ 4 ]
+	I[ 4, 5 ] = - inertial_coefficients[ 5 ]
+	I[ 4, 3 ] = - inertial_coefficients[ 3 ]
+	I[ 5, 3 ] = - inertial_coefficients[ 4 ]
+	I[ 5, 4 ] = - inertial_coefficients[ 5 ]
 
 	Phi, Theta, Psi = state[ 3 ], state[ 4 ], state[ 5 ]
 	cPhi, sPhi = np.cos( Phi ), np.sin( Phi )
 	cTheta, sTheta, tTheta = np.cos( Theta ), np.sin( Theta ), np.tan( Theta )
 	cPsi, sPsi = np.cos( Psi ), np.sin( Psi )
+	J = casadi.SX.zeros( (6, 6) )
+	J[ 0, :3 ] = np.array(
+			[ cPsi * cTheta, -sPsi * cPhi + cPsi * sTheta * sPhi,
+				sPsi * sPhi + cPsi * sTheta * cPhi ]
+			)
+	J[ 1, :3 ] = np.array(
+			[ sPsi * cTheta, cPsi * cPhi + sPsi * sTheta * sPhi,
+				-cPsi * sPhi + sPsi * sTheta * cPhi ]
+			)
+	J[ 2, :3 ] = np.array( [ -sTheta, cTheta * sPhi, cTheta * cPhi ] )
+	J[ 3, 3: ] = np.array( [ 1, sPhi * tTheta, cPhi * tTheta ] )
+	J[ 4, 3: ] = np.array( [ 0, cPhi, -sPhi ] )
+	J[ 5, 3: ] = np.array( [ 0, sPhi / cTheta, cPhi / cTheta ] )
 
-	J = np.zeros( (6, 6) )
-	J[ 0, :3 ] = [ cPsi * cTheta, -sPsi * cPhi + cPsi * sTheta * sPhi,
-								 sPsi * sPhi + cPsi * sTheta * cPhi ]
-	J[ 1, :3 ] = [ sPsi * cTheta, cPsi * cPhi + sPsi * sTheta * sPhi,
-								 -cPsi * sPhi + sPsi * sTheta * cPhi ]
-	J[ 2, :3 ] = [ -sTheta, cTheta * sPhi, cTheta * cPhi ]
-	J[ 3, 3: ] = [ 1, sPhi * tTheta, cPhi * tTheta ]
-	J[ 4, 3: ] = [ 0, cPhi, -sPhi ]
-	J[ 5, 3: ] = [ 0, sPhi / cTheta, cPhi / cTheta ]
 	hydrodynamic_coefficients = robot_configuration[ "hydrodynamic_coefficients" ]
 	D = np.multiply( np.eye( 6 ), hydrodynamic_coefficients[ :6 ] ) + np.multiply(
 			np.eye( 6 ), hydrodynamic_coefficients[ 6: ] * norm_2( state[ 6: ] )
 			)
-
-	A = casadi.SX( np.zeros( (12, 12) ) )
-	A[ 6:, :6 ] = J
-	A[ 6:, 6: ] = D
-
 	buoyancy = robot_configuration[ "buoyancy" ]
 	center_of_volume = robot_configuration[ "center_of_volume" ]
 	Fw = mass * np.array( [ 0, 0, 9.80665 ] )
 	Fb = buoyancy * np.array( [ 0, 0, 1 ] )
-	b = casadi.SX( np.zeros( 12 ) )
-	b[ 6:9 ] = J[ :3, :3 ].T @ (Fw + Fb)
-	b[ 9: ] = J[ :3, :3 ].T @ (np.cross( center_of_mass, Fw ) + np.cross( center_of_volume, Fb ))
+	S = casadi.SX( np.zeros( 6 ) )
+	S[ :3 ] = J[ :3, :3 ].T @ (Fw + Fb)
+	S[ 3: ] = J[ :3, :3 ].T @ (np.cross( center_of_mass, Fw ) + np.cross( center_of_volume, Fb ))
 
-	return I, A, b
+	return J, I, D, S
 
+dt = 0.01
+horizon = 100
 
-if __name__ == '__main__':
-	import importlib.util
-	import do_mpc
+eta = model.set_variable( '_x', 'eta', shape = (6, 1) )
+deta = model.set_variable( '_x', 'deta', shape = (6, 1) )
+nu = model.set_variable( '_x', 'nu', shape = (6, 1) )
+dnu = model.set_variable( '_x', 'dnu', shape = (6, 1) )
 
-	model = do_mpc.model.Model( 'continuous' )
+u = model.set_variable( '_u', 'force', shape = (6, 1) )
 
-	state = model.set_variable( var_type = '_x', var_name = 'state', shape = (12, 1) )
-	actuation = model.set_variable( var_type = '_u', var_name = 'actuation', shape = (6, 1) )
-	# dactuation = model.set_variable( var_type = '_x', var_name = 'dactuation', shape = (6, 1) )
+cost = model.set_expression('cost', sum1((casadi.SX(np.array([0, 0, 1, 0, 0, 0])) - eta) ** 2))
 
-	dt = 0.01
+J, I, D, S = get_state_matrixes(eta)
 
-	I, A, b = compute_IAb( state )
-	u = casadi.SX( np.zeros( 12 ) )
-	u[ 6: ] = actuation
-	dstate = casadi.inv( I ) @ (A @ state + b + u)
+model.set_rhs( 'deta', J @ nu )
+model.set_rhs( 'dnu', casadi.inv(I) @ (D @ eta + S + u) )
+model.set_rhs('eta', eta + deta * dt)
+model.set_rhs('nu', nu + dnu * dt)
 
-	model.set_rhs( 'state', dstate )
-	# model.set_rhs( 'dactuation', casadi.diff( actuation ) )
+model.setup()
 
-	model.set_expression( expr_name = 'cost', expr = sum1( state ** 2 ) )
+mpc = do_mpc.controller.MPC( model )
 
-	# print(model.x)
-	# print(model.u)
-	# print(model.rhs_list)
+setup_mpc = {
+		'n_horizon'           : horizon,
+		'n_robust'            : horizon // 4,
+		'open_loop'           : 0,
+		't_step'              : dt,
+		'state_discretization': 'collocation',
+		'collocation_type'    : 'radau',
+		'collocation_deg'     : 3,
+		'collocation_ni'      : 1,
+		'store_full_solution' : True,  # Use MA27 linear solver in ipopt for faster calculations:
+		'nlpsol_opts'         : { 'ipopt.linear_solver': 'mumps' }
+		}
+mpc.set_param( **setup_mpc )
+mpc.settings.supress_ipopt_output()
 
-	model.setup()
+mterm = model.aux[ 'cost' ]
+lterm = model.aux[ 'cost' ]
 
-	mpc = do_mpc.controller.MPC( model )
+mpc.set_objective( mterm = mterm, lterm = lterm )
+# Input force is implicitly restricted through the objective.
+mpc.set_rterm( force = 1e-9 )
 
-	setup_mpc = {
-			'n_robust'            : 5,
-			'n_horizon'           : 20,
-			't_step'              : dt,
-			'state_discretization': 'collocation',
-			'collocation_type'    : 'radau',
-			'collocation_deg'     : 3,
-			'collocation_ni'      : 1,
-			# 'nlpsol_opts'         : { 'ipopt.linear_solver': 'MA27' }
-			}
+# bounds on force
+mpc.bounds[ 'lower', '_u', 'force' ] = - bluerov_configuration["robot_max_actuation"]
+mpc.bounds[ 'upper', '_u', 'force' ] = bluerov_configuration["robot_max_actuation"]
 
-	mpc.set_param( **setup_mpc )
-	mpc.set_objective( mterm = model.aux[ 'cost' ], lterm = model.aux[ 'cost' ] )
-	mpc.set_rterm( actuation = 1e-5 )
+mpc.prepare_nlp()
 
-	mpc.bounds[ 'lower', '_u', 'actuation' ] = - bluerov_configuration[ "robot_max_actuation" ]
-	mpc.bounds[ 'upper', '_u', 'actuation' ] = bluerov_configuration[ "robot_max_actuation" ]
+# bounds on force derivative
+for i in range(horizon-1):
+	for j in range(6):
+		mpc.nlp_cons.append((mpc.opt_x['_u', i + 1, 0][j] - mpc.opt_x['_u', i, 0][j]) / dt)
+		mpc.nlp_cons_lb.append(- bluerov_configuration['robot_max_actuation_ramp'][j])
+		mpc.nlp_cons_ub.append(bluerov_configuration['robot_max_actuation_ramp'][j])
 
-	# mpc.bounds[ 'lower', '_x', 'dactuation' ] = -
-	# bluerov_configuration[	# 	"robot_max_actuation_ramp" ] * dt	# mpc.bounds[ 'upper', '_x',
-	# 'dactuation' ] = bluerov_configuration[	#
-	# "robot_max_actuation_ramp" ] * dt
+mpc.create_nlp()
 
-	mpc.setup()
+mpc.setup()
 
-	params_simulator = {
-			'integration_tool': 'idas',
-			'abstol'          : 1e-8,
-			'reltol'          : 1e-8,
-			't_step'          : 0.04
-			}
-	simulator = do_mpc.simulator.Simulator( model )
-	simulator.set_param( **params_simulator )
-	simulator.setup()
+estimator = do_mpc.estimator.StateFeedback( model )
 
-	x = np.array( [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ] )
-	mpc.x0 = x
-	simulator.x0 = x
+simulator = do_mpc.simulator.Simulator( model )
+params_simulator = {
+		# Note: cvode doesn't support DAE systems.
+		'integration_tool': 'idas', 'abstol': 1e-8, 'reltol': 1e-8, 't_step': dt
+		}
+simulator.set_param( **params_simulator )
 
-	mpc.set_initial_guess()
+simulator.setup()
 
-	for k in range( 100 ):
-		u = mpc.make_step( x )
-		x = simulator.make_step( u )
+simulator.x0['eta'] = np.zeros(6)
+simulator.x0['nu'] = np.zeros(6)
 
-		print( x )
-		print( u )
+x0 = simulator.x0
+mpc.x0 = x0
+estimator.x0 = x0
+
+mpc.set_initial_guess()
+
+nsteps = 100
+for i in range( 1, nsteps + 1 ):
+	print( f'{i = } / {nsteps = }' )
+	u0 = mpc.make_step( x0 )
+	y0 = estimator.make_step( u0 )
+	x0 = simulator.make_step( y0 )
+	print( f'{x0[:6].T = }' )
+	print( f'{x0[6:12].T = }' )
+	print( f'{x0[12:18].T = }' )
+	print( f'{x0[18:].T = }' )
+	print( f'{y0.T = }' )
+	print( f'{u0.T = }' )
