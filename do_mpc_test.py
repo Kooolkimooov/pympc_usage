c5332ffc2a90c1b9ba4099228522bcd45fd17b35
martin filliung <martin-filliung@etud.univ-tln.fr>
2024-02-28 10:09:46 +0100

    seems to work

--- do_mpc_test.py
+++ do_mpc_test.py
@@ -1,10 +1,15 @@
 import importlib.util
+import time
+
 import numpy as np
 import sys
 from casadi import *
 
 # Import do_mpc package:
 import do_mpc
+from matplotlib import pyplot as plt
+
+ti = time.perf_counter()
 
 model_type = 'continuous'  # either 'discrete' or 'continuous'
 model = do_mpc.model.Model( model_type )
@@ -18,9 +23,35 @@ bluerov_configuration = {
 		"hydrodynamic_coefficients": np.array(
 				[ 4.03, 6.22, 5.18, 0.07, 0.07, 0.07, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 ]
 				),
-		"robot_max_actuation"      : np.array( [ 100, 100, 100, 100, 100, 100 ] ),
+		"robot_max_actuation"      : np.array( [ 1000, 1000, 1000, 1000, 1000, 1000 ] ),
 		"robot_max_actuation_ramp" : np.array( [ 100, 100, 100, 100, 100, 100 ] )
 		}
+
+horizon = 50
+r_horizon = 5
+nsteps = 10
+dt = 0.5
+
+setup_mpc = {
+		'n_horizon'           : horizon,
+		'n_robust'            : r_horizon,
+		'open_loop'           : 0,
+		't_step'              : dt,
+		'state_discretization': 'collocation',
+		'collocation_type'    : 'radau',
+		'collocation_deg'     : 3,
+		'collocation_ni'      : 1,
+		'store_full_solution' : True,
+
+		# Use MA27 linear solver in ipopt for faster calculations:
+		'nlpsol_opts'         : { 'ipopt.linear_solver': 'mumps' }
+		}
+
+params_simulator = {
+		# Note: cvode doesn't support DAE systems.
+		'integration_tool': 'idas', 'abstol': 1e-8, 'reltol': 1e-8, 't_step': dt
+		}
+
 
 def get_state_matrixes( state, robot_configuration = None ):
 	if robot_configuration is None:
@@ -57,12 +88,10 @@ def get_state_matrixes( state, robot_configuration = None ):
 	cPsi, sPsi = np.cos( Psi ), np.sin( Psi )
 	J = casadi.SX.zeros( (6, 6) )
 	J[ 0, :3 ] = np.array(
-			[ cPsi * cTheta, -sPsi * cPhi + cPsi * sTheta * sPhi,
-				sPsi * sPhi + cPsi * sTheta * cPhi ]
+			[ cPsi * cTheta, -sPsi * cPhi + cPsi * sTheta * sPhi, sPsi * sPhi + cPsi * sTheta * cPhi ]
 			)
 	J[ 1, :3 ] = np.array(
-			[ sPsi * cTheta, cPsi * cPhi + sPsi * sTheta * sPhi,
-				-cPsi * sPhi + sPsi * sTheta * cPhi ]
+			[ sPsi * cTheta, cPsi * cPhi + sPsi * sTheta * sPhi, -cPsi * sPhi + sPsi * sTheta * cPhi ]
 			)
 	J[ 2, :3 ] = np.array( [ -sTheta, cTheta * sPhi, cTheta * cPhi ] )
 	J[ 3, 3: ] = np.array( [ 1, sPhi * tTheta, cPhi * tTheta ] )
@@ -83,8 +112,6 @@ def get_state_matrixes( state, robot_configuration = None ):
 
 	return J, I, D, S
 
-dt = 0.01
-horizon = 100
 
 eta = model.set_variable( '_x', 'eta', shape = (6, 1) )
 deta = model.set_variable( '_x', 'deta', shape = (6, 1) )
@@ -93,31 +120,20 @@ dnu = model.set_variable( '_x', 'dnu', shape = (6, 1) )
 
 u = model.set_variable( '_u', 'force', shape = (6, 1) )
 
-cost = model.set_expression('cost', sum1((casadi.SX(np.array([0, 0, 1, 0, 0, 0])) - eta) ** 2))
+cost = model.set_expression(
+		'cost', sum1( (casadi.SX( np.array( [ 0, 0, 1, 0, 0, 0 ] ) ) - eta) ** 2 )
+		)
 
-J, I, D, S = get_state_matrixes(eta)
+J, I, D, S = get_state_matrixes( eta )
 
 model.set_rhs( 'deta', J @ nu )
-model.set_rhs( 'dnu', casadi.inv(I) @ (D @ eta + S + u) )
-model.set_rhs('eta', eta + deta * dt)
-model.set_rhs('nu', nu + dnu * dt)
+model.set_rhs( 'dnu', casadi.inv( I ) @ (D @ eta + S + u) )
+model.set_rhs( 'eta', deta )
+model.set_rhs( 'nu', dnu )
 
 model.setup()
 
 mpc = do_mpc.controller.MPC( model )
-
-setup_mpc = {
-		'n_horizon'           : horizon,
-		'n_robust'            : horizon // 4,
-		'open_loop'           : 0,
-		't_step'              : dt,
-		'state_discretization': 'collocation',
-		'collocation_type'    : 'radau',
-		'collocation_deg'     : 3,
-		'collocation_ni'      : 1,
-		'store_full_solution' : True,  # Use MA27 linear solver in ipopt for faster calculations:
-		'nlpsol_opts'         : { 'ipopt.linear_solver': 'mumps' }
-		}
 mpc.set_param( **setup_mpc )
 mpc.settings.supress_ipopt_output()
 
@@ -125,55 +141,69 @@ mterm = model.aux[ 'cost' ]
 lterm = model.aux[ 'cost' ]
 
 mpc.set_objective( mterm = mterm, lterm = lterm )
-# Input force is implicitly restricted through the objective.
-mpc.set_rterm( force = 1e-9 )
+mpc.set_rterm( force = 0 )
 
 # bounds on force
-mpc.bounds[ 'lower', '_u', 'force' ] = - bluerov_configuration["robot_max_actuation"]
-mpc.bounds[ 'upper', '_u', 'force' ] = bluerov_configuration["robot_max_actuation"]
+mpc.bounds[ 'lower', '_u', 'force' ] = - bluerov_configuration[ "robot_max_actuation" ]
+mpc.bounds[ 'upper', '_u', 'force' ] = bluerov_configuration[ "robot_max_actuation" ]
 
 mpc.prepare_nlp()
-
 # bounds on force derivative
-for i in range(horizon-1):
-	for j in range(6):
-		mpc.nlp_cons.append((mpc.opt_x['_u', i + 1, 0][j] - mpc.opt_x['_u', i, 0][j]) / dt)
-		mpc.nlp_cons_lb.append(- bluerov_configuration['robot_max_actuation_ramp'][j])
-		mpc.nlp_cons_ub.append(bluerov_configuration['robot_max_actuation_ramp'][j])
-
-mpc.create_nlp()
-
+for i in range( horizon - 1 ):
+	for j in range( 6 ):
+		mpc.nlp_cons.append( (mpc.opt_x[ '_u', i + 1, 0 ][ j ] - mpc.opt_x[ '_u', i, 0 ][ j ]) )
+		mpc.nlp_cons_lb.append( - bluerov_configuration[ 'robot_max_actuation_ramp' ][ j ] * dt )
+		mpc.nlp_cons_ub.append( bluerov_configuration[ 'robot_max_actuation_ramp' ][ j ] * dt )
 mpc.setup()
 
 estimator = do_mpc.estimator.StateFeedback( model )
-
 simulator = do_mpc.simulator.Simulator( model )
-params_simulator = {
-		# Note: cvode doesn't support DAE systems.
-		'integration_tool': 'idas', 'abstol': 1e-8, 'reltol': 1e-8, 't_step': dt
-		}
 simulator.set_param( **params_simulator )
-
 simulator.setup()
 
-simulator.x0['eta'] = np.zeros(6)
-simulator.x0['nu'] = np.zeros(6)
+simulator.x0[ 'eta' ] = np.zeros( 6 )
+simulator.x0[ 'nu' ] = np.zeros( 6 )
 
 x0 = simulator.x0
 mpc.x0 = x0
+mpc.u0 = np.zeros( 6 )
 estimator.x0 = x0
-
 mpc.set_initial_guess()
 
-nsteps = 100
+sim_graphics = do_mpc.graphics.Graphics( simulator.data )
+fig, ax = plt.subplots( 3, sharex = True, figsize = (16, 9) )
+fig.align_ylabels()
+sim_graphics.add_line( var_type = '_x', var_name = 'eta', axis = ax[ 0 ] )
+sim_graphics.add_line( var_type = '_u', var_name = 'force', axis = ax[ 1 ] )
+
+computation_times = [ ]
+
+tf = time.perf_counter()
+print( f'initialization in {tf - ti = }' )
+
 for i in range( 1, nsteps + 1 ):
-	print( f'{i = } / {nsteps = }' )
+	print( f' -> {dt = } s | {horizon = } | {i = } / {nsteps} | {i * dt = :.3f} s' )
+	ti = time.perf_counter()
 	u0 = mpc.make_step( x0 )
-	y0 = estimator.make_step( u0 )
-	x0 = simulator.make_step( y0 )
-	print( f'{x0[:6].T = }' )
-	print( f'{x0[6:12].T = }' )
-	print( f'{x0[12:18].T = }' )
-	print( f'{x0[18:].T = }' )
-	print( f'{y0.T = }' )
-	print( f'{u0.T = }' )
+	y0 = simulator.make_step( u0 )
+	x0 = estimator.make_step( y0 )
+	tf = time.perf_counter()
+	computation_times += [ tf - ti ]
+	print(
+			f'\tdt = {tf - ti:.3f} s | '
+			f'median = {np.median( computation_times ):.3f} s | '
+			f'min = {min( computation_times ):.3f} s | '
+			f'1% = {np.percentile( computation_times, 1 ):.3f} s | '
+			f'25% = {np.percentile( computation_times, 25 ):.3f} s | '
+			f'75% = {np.percentile( computation_times, 75 ):.3f} s | '
+			f'99% = {np.percentile( computation_times, 99 ):.3f} s | '
+			f'max = {max( computation_times ):.3f} s | '
+			f'total time = {sum( computation_times ):.3f} s'
+			)
+	print( '\tu =', str( u0 ).replace( '\n', '' ) )
+	print( '\tx =', str( x0[ :6 ] ).replace( '\n', '' ) )
+
+ax[ 2 ].plot( [ k * dt for k in range( nsteps ) ], computation_times )
+sim_graphics.plot_results()
+sim_graphics.reset_axes()
+plt.show()
