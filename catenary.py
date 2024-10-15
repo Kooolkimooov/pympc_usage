868a8e2805a0665916dc7085c8015674c76d8546
martin filliung <martin-filliung@etud.univ-tln.fr>
2024-10-15 13:55:36 +0200

    change: rename p1 and p2 to p0 and p1 respectively for consistency

--- catenary.py
+++ catenary.py
@@ -2,7 +2,20 @@ from json import dump, load
 from pathlib import Path
 
 from numpy import (
-	arccosh, arcsinh, array, cosh, isnan, linspace, log10, logspace, meshgrid, ndarray, sinh, sqrt, zeros,
+	arccosh,
+	arcsinh,
+	array,
+	cosh,
+	isnan,
+	linspace,
+	log10,
+	logspace,
+	meshgrid,
+	ndarray,
+	set_printoptions,
+	sinh,
+	sqrt,
+	zeros,
 	)
 from numpy.linalg import norm
 from scipy.optimize import brentq
@@ -24,7 +37,7 @@ class Catenary:
 
 		self.length = length
 		self.linear_mass = linear_mass
-		self.optimization_function = self._optimization_function_1
+		self.optimization_function = self._optimization_function_0
 
 		match get_parameter_method:
 			case 'runtime':
@@ -35,12 +48,12 @@ class Catenary:
 			case _:
 				raise ValueError( f'get_parameter_method must be one of {self.GET_PARAMETER_METHOD}' )
 
-	def __call__( self, p1: ndarray, p2: ndarray ):
+	def __call__( self, p0: ndarray, p1: ndarray ):
 		"""
 		get all relevant data on the catenary of length self.length, linear mass self.linear_mass, and the given
 		attachment points
-		:param p1: first attachment point
-		:param p2: second attachment point
+		:param p0: first attachment point
+		:param p1: second attachment point
 		:return: tuple containing:
 		- the parameters of the catenary:
 			- the parameter of the catenary (C, set to None if out of safe search space);
@@ -50,55 +63,55 @@ class Catenary:
 			- horizontal half-length (D, set to None if out of safe search space and 2D+ΔD > length);
 			- horizontal asymmetric length (ΔD, set to None if out of safe search space and 2D+ΔD > length);
 		- the lowest point (x, y, z) of the catenary;
-		- the perturbations force on the two points in the form (perturbation_p1, perturbation_p2);
+		- the perturbations force on the two points in the form (perturbation_p0, perturbation_p1);
 		- array of points for plotting (x, y, z) are on the second dimension of the array.
 		"""
-		C, H, dH, D, dD = self.get_parameters( p1, p2 )
-		lowest_point = self._get_lowest_point( p1, p2, C, H, dH, D, dD )
-		perturbations = self._get_perturbations( p1, p2, C, H, dH, D, dD )
-		points = self._discretize( p1, p2, C, H, D, dD )
+		C, H, dH, D, dD = self.get_parameters( p0, p1 )
+		lowest_point = self._get_lowest_point( p0, p1, C, H, dH, D, dD )
+		perturbations = self._get_perturbations( p0, p1, C, H, dH, D, dD )
+		points = self._discretize( p0, p1, C, H, D, dD )
 
 		return (C, H, dH, D, dD), lowest_point, perturbations, points
 
-	def get_lowest_point( self, p1: ndarray, p2: ndarray ) -> ndarray:
+	def get_lowest_point( self, p0: ndarray, p1: ndarray ) -> ndarray:
 		"""
 		get the catenary's lowest point
-		:param p1: one end of the catenary
-		:param p2: second end of the catenary
+		:param p0: one end of the catenary
+		:param p1: second end of the catenary
 		:return: the lowest point (x, y, z) of the catenary
 		"""
-		C, H, dH, D, dD = self.get_parameters( p1, p2 )
-		return self._get_lowest_point( p1, p2, C, H, dH, D, dD )
+		C, H, dH, D, dD = self.get_parameters( p0, p1 )
+		return self._get_lowest_point( p0, p1, C, H, dH, D, dD )
 
-	def get_perturbations( self, p1: ndarray, p2: ndarray ):
+	def get_perturbations( self, p0: ndarray, p1: ndarray ):
 		"""
 		get the perturbations of the two points
-		:param p1: one end of the catenary
-		:param p2: second end of the catenary
+		:param p0: one end of the catenary
+		:param p1: second end of the catenary
 		:return: tuple containing the perturbations force on the two points in the form (perturbation_p1, perturbation_p2)
 		"""
-		C, H, dH, D, dD = self.get_parameters( p1, p2 )
-		return self._get_perturbations( p1, p2, C, H, dH, D, dD )
+		C, H, dH, D, dD = self.get_parameters( p0, p1 )
+		return self._get_perturbations( p0, p1, C, H, dH, D, dD )
 
-	def discretize( self, p1: ndarray, p2: ndarray, n: int = 100 ) -> ndarray:
+	def discretize( self, p0: ndarray, p1: ndarray, n: int = 100 ) -> ndarray:
 		"""
 		discretize the catenary, if the optimization fails, the catenary is approximated by a straight line
-		:param p1: one end of the catenary
-		:param p2: second end of the catenary
+		:param p0: one end of the catenary
+		:param p1: second end of the catenary
 		:param n: number of point to discretize
 		:return: array of points of the catenary points are on the second dimension of the array
 		"""
-		C, H, dH, D, dD = self.get_parameters( p1, p2 )
-		return self._discretize( p1, p2, C, H, D, dD, n )
+		C, H, dH, D, dD = self.get_parameters( p0, p1 )
+		return self._discretize( p0, p1, C, H, D, dD, n )
 
 	@staticmethod
 	def optimization_function( C, length, dH, two_D_plus_dD ):
 		raise NotImplementedError( 'optimization_function method should have been implemented in __init__' )
 
-	def get_parameters( self, p1: ndarray, p2: ndarray ) -> tuple[ float, float, float, float, float ]:
+	def get_parameters( self, p0: ndarray, p1: ndarray ) -> tuple[ float, float, float, float, float ]:
 		"""
-		:param p1: first attachment point
-		:param p2: second attachment point
+		:param p0: first attachment point
+		:param p1: second attachment point
 		:return: tuple containing:
 		- the parameter of the catenary (C, set to None if out of safe search space);
 		- vertical sag of the catenary (H, set to None if out of safe search space and 2D+ΔD > length);
@@ -108,15 +121,15 @@ class Catenary:
 		"""
 		raise NotImplementedError( 'get_parameters method should have been implemented in __init__' )
 
-	def _get_parameters_optimization( self, p1: ndarray, p2: ndarray ) -> tuple[ float, float, float, float, float ]:
+	def _get_parameters_optimization( self, p0: ndarray, p1: ndarray ) -> tuple[ float, float, float, float, float ]:
 		"""
 		implementation of get_parameters using optimization
 		"""
 
-		dH = p2[ 2 ] - p1[ 2 ]
-		two_D_plus_dD = norm( p2[ :2 ] - p1[ :2 ] )
+		dH = p1[ 2 ] - p0[ 2 ]
+		two_D_plus_dD = norm( p1[ :2 ] - p0[ :2 ] )
 
-		if norm( p2 - p1 ) > 0.99 * self.length or any( isnan( p1 ) ) or any( isnan( p2 ) ):
+		if norm( p1 - p0 ) > 0.99 * self.length or any( isnan( p0 ) ) or any( isnan( p1 ) ):
 			return None, None, dH, None, None
 		elif two_D_plus_dD < .01 * self.length:
 			return None, (self.length - dH) / 2, dH, two_D_plus_dD / 2, 0.
@@ -162,14 +175,14 @@ class Catenary:
 		with open( Path( f'./cache/Catenary/{self.length}.json' ), 'w' ) as file:
 			dump( self._Cs.tolist(), file )
 
-	def _get_parameters_precompute( self, p1: ndarray, p2: ndarray ) -> tuple[ float, float, float, float, float ]:
+	def _get_parameters_precompute( self, p0: ndarray, p1: ndarray ) -> tuple[ float, float, float, float, float ]:
 		"""
 		implementation of get_parameters using optimization
 		"""
-		dH = p2[ 2 ] - p1[ 2 ]
-		two_D_plus_dD = norm( p2[ :2 ] - p1[ :2 ] )
+		dH = p1[ 2 ] - p0[ 2 ]
+		two_D_plus_dD = norm( p1[ :2 ] - p0[ :2 ] )
 
-		if norm( p2 - p1 ) > 0.99 * self.length or any( isnan( p1 ) ) or any( isnan( p2 ) ):
+		if norm( p1 - p0 ) > 0.99 * self.length or any( isnan( p0 ) ) or any( isnan( p1 ) ):
 			return None, None, dH, None, None
 		elif two_D_plus_dD < .01 * self.length:
 			return None, (self.length - dH) / 2, dH, two_D_plus_dD / 2, 0.
@@ -199,22 +212,22 @@ class Catenary:
 
 		return C, H, dH, D, dD
 
-	def _get_lowest_point( self, p1: ndarray, p2: ndarray, C: float, H: float, dH: float, D: float, dD: float ):
+	def _get_lowest_point( self, p0: ndarray, p1: ndarray, C: float, H: float, dH: float, D: float, dD: float ):
 
 		# case where horizontal distance is too small
 		if (C is None) and (H is not None):
-			return p1 + array( [ 0, 0, H + dH ] )
+			return p0 + array( [ 0, 0, H + dH ] )
 		# case where cable is taunt
 		elif C is None:
-			return p1 if p1[ 2 ] >= p2[ 2 ] else p2
+			return p0 if p0[ 2 ] >= p1[ 2 ] else p1
 
 		lowest_point = zeros( (3,) )
-		lowest_point[ :2 ] = p1[ :2 ] + (D + dD) * (p2[ :2 ] - p1[ :2 ]) / norm( p2[ :2 ] - p1[ :2 ] )
-		lowest_point[ 2 ] = p1[ 2 ] + H + dH
+		lowest_point[ :2 ] = p0[ :2 ] + (D + dD) * (p1[ :2 ] - p0[ :2 ]) / norm( p1[ :2 ] - p0[ :2 ] )
+		lowest_point[ 2 ] = p0[ 2 ] + H + dH
 		return lowest_point
 
 	def _get_perturbations(
-			self, p1: ndarray, p2: ndarray, C: float, H: float, dH: float, D: float, dD: float
+			self, p0: ndarray, p1: ndarray, C: float, H: float, dH: float, D: float, dD: float
 			) -> tuple[ ndarray, ndarray ]:
 
 		# case where horizontal distance is too small
@@ -229,27 +242,27 @@ class Catenary:
 			return None, None
 
 		horizontal_perturbation = self.linear_mass * G / C
-		vertical_perturbation_1 = horizontal_perturbation * sinh( -C * (D + dD) )
-		vertical_perturbation_2 = horizontal_perturbation * sinh( C * D )
+		vertical_perturbation_0 = horizontal_perturbation * sinh( -C * (D + dD) )
+		vertical_perturbation_1 = horizontal_perturbation * sinh( C * D )
 
-		direction = (p2[ :2 ] - p1[ :2 ]) / norm( p2[ :2 ] - p1[ :2 ] )
+		direction = (p1[ :2 ] - p0[ :2 ]) / norm( p1[ :2 ] - p0[ :2 ] )
 
-		perturbation_p1, perturbation_p2 = zeros( (3,) ), zeros( (3,) )
-		perturbation_p1[ :2 ] = direction * horizontal_perturbation
+		perturbation_p0, perturbation_p1 = zeros( (3,) ), zeros( (3,) )
+		perturbation_p0[ :2 ] = direction * horizontal_perturbation
+		perturbation_p0[ 2 ] = -vertical_perturbation_0
+		perturbation_p1[ :2 ] = -direction * horizontal_perturbation
 		perturbation_p1[ 2 ] = vertical_perturbation_1
-		perturbation_p2[ :2 ] = -direction * horizontal_perturbation
-		perturbation_p2[ 2 ] = -vertical_perturbation_2
 
-		return perturbation_p1, perturbation_p2
+		return perturbation_p0, perturbation_p1
 
-	def _discretize( self, p1: ndarray, p2: ndarray, C: float, H: float, D: float, dD: float, n: int = 100 ) -> ndarray:
+	def _discretize( self, p0: ndarray, p1: ndarray, C: float, H: float, D: float, dD: float, n: int = 100 ) -> ndarray:
 
 		# case where ΔH is too small
 		if (C is None) and (D is not None):
-			return array( [ p1, p1 + array( [ 0, 0, H ] ), p2 ] )
+			return array( [ p0, p0 + array( [ 0, 0, H ] ), p1 ] )
 		# case where cable is taunt
 		elif C is None:
-			return array( [ p1, p2 ] )
+			return array( [ p0, p1 ] )
 
 		points = zeros( (100, 3) )
 
@@ -261,21 +274,21 @@ class Catenary:
 			inter = C * s - sinh( C * D )
 			x = 1. / C * arcsinh( inter ) + D
 			z = 1. / C * (sqrt( 1. + pow( inter, 2 ) ) - 1.) - H
-			points[ i ][ 0 ] = p2[ 0 ] - x * (p2[ 0 ] - p1[ 0 ]) / (2. * D + dD)
-			points[ i ][ 1 ] = p2[ 1 ] - x * (p2[ 1 ] - p1[ 1 ]) / (2. * D + dD)
-			points[ i ][ 2 ] = p2[ 2 ] - z
+			points[ i, 0 ] = p1[ 0 ] - x * (p1[ 0 ] - p0[ 0 ]) / (2. * D + dD)
+			points[ i, 1 ] = p1[ 1 ] - x * (p1[ 1 ] - p0[ 1 ]) / (2. * D + dD)
+			points[ i, 2 ] = p1[ 2 ] - z
 			s += ds
 
-		points[ -1 ] = p1
+		points[ -1 ] = p0
 
 		return points
 
 	@staticmethod
-	def _optimization_function_1( C, length, dH, two_D_plus_dD ):
+	def _optimization_function_0( C, length, dH, two_D_plus_dD ):
 		return C * C * (length * length - dH * dH) - 2.0 * (-1.0 + cosh( C * two_D_plus_dD ))
 
 	@staticmethod
-	def _optimization_function_2( C, length, dH, two_D_plus_dD ):
+	def _optimization_function_1( C, length, dH, two_D_plus_dD ):
 		return pow( length, 2 ) - pow( dH, 2 ) - pow( 2 * sinh( C * two_D_plus_dD / 2 ) / C, 2 )
 
 
@@ -292,8 +305,8 @@ def test_1():
 	simplefilter( 'ignore', RuntimeWarning )
 
 	cat = Catenary()
-	# cat.optimization_function = cat._optimization_function_1 # μ=0.0004014649499549705 σ=0.0002544858910793164
-	# cat.optimization_function = cat._optimization_function_2  # μ=0.00039219858003561965 σ=0.00023520437741971333
+	# cat.optimization_function = cat._optimization_function_0 # μ=0.0004014649499549705 σ=0.0002544858910793164
+	# cat.optimization_function = cat._optimization_function_1  # μ=0.00039219858003561965 σ=0.00023520437741971333
 	lengths = linspace( 3., 10., 1 )
 
 	xlpisp = [ ]
@@ -399,15 +412,20 @@ def test_2():
 				p1 = array( [ 0, 0, 0 ] )
 				p2 = array( [ x, 0, z ] )
 
-				plt.scatter( *p1[ ::2 ], s = 50 )
-				plt.scatter( *p2[ ::2 ], s = 50 )
-
 				out = cat( p1, p2 )
+
 				C, H, dH, D, dD = out[ 0 ]
 				lowest_point = out[ 1 ]
 				perturbations = out[ 2 ]
 				points = out[ 3 ]
 
+				plt.figure()
+				plt.gca().set_xlim([-3, 3])
+				plt.gca().set_ylim([-3, 3])
+				plt.gca().invert_yaxis()
+
+				plt.scatter( *p1[ ::2 ], s = 50 )
+				plt.scatter( *p2[ ::2 ], s = 50 )
 				plt.scatter( *lowest_point[ ::2 ], s = 50 )
 
 				plt.quiver( *p1[ ::2 ], *perturbations[ 0 ][ ::2 ], angles='xy', scale = 50 )
@@ -434,9 +452,7 @@ def test_2():
 				plt.plot( [ D + dD, 2 * D + dD ], [ H + dH, dH ], ':', linewidth = 5 )
 
 				plt.title( f'{p2=}' )
-				print( f'{p2=}' )
-				plt.gca().invert_yaxis()
-				plt.axis( 'equal' )
+				print( perturbations )
 				plt.show()
 
 			except:
@@ -444,7 +460,7 @@ def test_2():
 
 
 def test_3():
-	from numpy import linspace, logspace, array
+	from numpy import linspace, array
 	from warnings import simplefilter
 	import matplotlib.pyplot as plt
 	from tqdm import tqdm
@@ -456,41 +472,26 @@ def test_3():
 	ds = [ ]
 	t1s, t2s = [ ], [ ]
 
-	z_offset = 1.
+	X = linspace( -cat.length, cat.length, 500 )
+	Z = linspace( -cat.length, cat.length, 500 )
+	X, Z = meshgrid( X, Z )
+	P1, P2 = zeros( X.shape ), zeros( X.shape )
 
-	distances = linspace( 0., cat.length, 2000 )
-	weights = logspace( -1, 1, 10 )
-	legends = [ ]
-
-	_, (ax1, ax2) = plt.subplots( 2, 1 )
-
-	for weight in weights:
-		cat.linear_mass = weight / cat.length
-		legends += [ f'{weight=:.3f}' ]
-		for distance in tqdm( distances, desc = f'{weight=:.3f}' ):
+	for i, xr in enumerate( tqdm( X ) ):
+		for j, x in enumerate( xr ):
+			z = Z[ i, j ]
 			p1 = array( [ 0, 0, 0 ] )
-			p2 = array( [ distance, 0, z_offset ] )
+			p2 = array( [ x, 0, z ] )
 			t1, t2 = cat.get_perturbations( p1, p2 )
 			if t1 is not None:
 				t1, t2 = norm( t1 ), norm( t2 )
-				t1s += [ t1 ]
-				t2s += [ t2 ]
-				ds += [ distance ]
+				P1[ i, j ] = t1
+				P2[ i, j ] = t2
 
-		ax1.plot( ds, t1s )
-		ax2.plot( ds, t2s )
-		ds.clear()
-		t1s.clear()
-		t2s.clear()
-	ax1.set_yscale( 'log' )
-	ax1.set_xlabel( 'distance [m]' )
-	ax1.set_ylabel( 'norme de la tension p1 [N]' )
-	ax1.legend( legends )
-	ax2.set_yscale( 'log' )
-	ax2.set_xlabel( 'distance [m]' )
-	ax2.set_ylabel( 'norme de la tension p2 [N]' )
-	ax2.legend( legends )
-	ax1.set_title( f'{z_offset=} for p2' )
+	_, (ax1, ax2) = plt.subplots( 2, 1, subplot_kw = { 'projection': '3d' } )
+
+	ax1.plot_surface( X, Z, P1 )
+	ax2.plot_surface( X, Z, P2 )
 	plt.show()
 
 
@@ -557,10 +558,11 @@ def test_5():
 
 
 if __name__ == '__main__':
+	set_printoptions( precision = 5, linewidth = 10000, suppress = True )
 	# test_1()
-	# test_2()
+	test_2()
 	# test_3()
-	test_4()
+	# test_4()
 	# test_5()
 	pass
 
