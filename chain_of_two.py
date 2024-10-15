1c06baf40cb9c8dcf3dfecf4eb8f785e4b262585
martin filliung <martin-filliung@etud.univ-tln.fr>
2024-10-15 13:55:50 +0200

    work in progress

--- chain_of_two.py
+++ chain_of_two.py
@@ -3,7 +3,7 @@ from time import perf_counter
 from warnings import simplefilter
 
 from matplotlib import pyplot as plt
-from numpy import any, dot, isnan, linspace, ndarray, r_, set_printoptions, zeros
+from numpy import any, array, dot, isnan, linspace, ndarray, r_, set_printoptions, zeros
 from numpy.linalg import norm
 from tqdm import tqdm
 
@@ -31,6 +31,9 @@ class ChainOf2:
 		self.br_0 = Bluerov( water_surface_z, water_current )
 		self.c_01 = Catenary( cables_lenght, cables_linear_mass, get_cable_parameter_method )
 		self.br_1 = Bluerov( water_surface_z, water_current )
+
+		self.last_perturbation_0 = zeros( (Bluerov.state_size // 2,) )
+		self.last_perturbation_1 = zeros( (Bluerov.state_size // 2,) )
 
 		self.br_0_pose = slice( 0, 6 )
 		self.br_0_position = slice( 0, 3 )
@@ -80,17 +83,20 @@ class ChainOf2:
 
 		# if the cable is taunt the perturbation is None
 		# here we should consider any pair with a taunt cable as a single body
-		if perturbation_01_0 is None:
-			perturbation_01_0, perturbation_01_1 = self.get_taunt_cable_perturbations( state, actuation, 0 )
-			perturbation_01_0.resize( (Bluerov.actuation_size,), refcheck = False )
-			perturbation_01_1.resize( (Bluerov.actuation_size,), refcheck = False )
+		if perturbation_01_0 is not None:
+			self.last_perturbation_0[ :3 ] = perturbation_01_0
+			self.last_perturbation_1[ :3 ] = perturbation_01_1
 		else:
-			perturbation_01_0.resize( (Bluerov.actuation_size,), refcheck = False )
-			perturbation_01_1.resize( (Bluerov.actuation_size,), refcheck = False )
-			br_0_transformation_matrix = self.br_0.build_transformation_matrix( *state[ self.br_0_orientation ] )
-			br_1_transformation_matrix = self.br_1.build_transformation_matrix( *state[ self.br_1_orientation ] )
-			perturbation_01_0 = br_0_transformation_matrix.T @ perturbation_01_0
-			perturbation_01_1 = br_1_transformation_matrix.T @ perturbation_01_1
+			perturbation_01_0, perturbation_01_1 = self.get_taunt_cable_perturbations( state, actuation, 0 )
+
+		perturbation_01_0.resize( (Bluerov.actuation_size,), refcheck = False )
+		perturbation_01_1.resize( (Bluerov.actuation_size,), refcheck = False )
+
+		# perturbation is in world frame, should be applied robot frame instead
+		br_0_transformation_matrix = self.br_0.build_transformation_matrix( *state[ self.br_0_orientation ] )
+		br_1_transformation_matrix = self.br_1.build_transformation_matrix( *state[ self.br_1_orientation ] )
+		perturbation_01_0 = br_0_transformation_matrix.T @ perturbation_01_0
+		perturbation_01_1 = br_1_transformation_matrix.T @ perturbation_01_1
 
 		state_derivative[ self.br_0_state ] = self.br_0(
 				state[ self.br_0_state ], actuation[ self.br_0_actuation ], perturbation_01_0
@@ -112,37 +118,40 @@ class ChainOf2:
 				br_0_position = self.br_0_position
 				br_0_orientation = self.br_0_orientation
 				br_0_actuation = self.br_0_actuation
+				br_0_perturbation = self.last_perturbation_0
 				br_1_state = self.br_1_state
 				br_1_position = self.br_1_position
 				br_1_orientation = self.br_1_orientation
 				br_1_actuation = self.br_1_actuation
+				br_1_perturbation = self.last_perturbation_1
 			case _:
 				raise RuntimeError( f'unknown {pair=}' )
 
+		# from br_0 to br_1
 		direction = state[ br_1_position ] - state[ br_0_position ]
 		direction /= norm( direction )
 
-		null_perturbation = zeros( (br_0.state_size // 2,) )
+		null = zeros( (br_0.state_size // 2,) )
 
 		br_0_transformation_matrix = br_0.build_transformation_matrix( *state[ br_0_orientation ] )
 		br_1_transformation_matrix = br_1.build_transformation_matrix( *state[ br_1_orientation ] )
 
-		br_0_acceleration = br_0( state[ br_0_state ], state[ br_0_actuation ], null_perturbation )[ 6: ]
+		# in robot frame
+		br_0_acceleration = br_0( state[ br_0_state ], actuation[ br_0_actuation ], null )[ 6: ]
 		br_0_forces = (br_0.inertial_matrix[ :3, :3 ] @ br_0_acceleration[ :3 ])
-		br_0_forces += br_0_acceleration[ :3 ] * c_01.length * c_01.linear_mass / 2
 
-		br_1_acceleration = br_1( state[ br_1_state ], state[ br_1_actuation ], null_perturbation )[ 6: ]
+		br_1_acceleration = br_1( state[ br_1_state ], actuation[ br_1_actuation ], null )[ 6: ]
 		br_1_forces = (br_1.inertial_matrix[ :3, :3 ] @ br_1_acceleration[ :3 ])
-		br_1_forces += br_1_acceleration[ :3 ] * c_01.length * c_01.linear_mass / 2
 
-		perturbation_01_0 = br_0_transformation_matrix[ :3, :3 ].T @ (direction * dot(
-				br_1_transformation_matrix[ :3, :3 ] @ br_1_forces, direction
-				))
-		perturbation_01_1 = br_1_transformation_matrix[ :3, :3 ].T @ (direction * dot(
-				br_0_transformation_matrix[ :3, :3 ] @ br_0_forces, direction
-				))
+		all_forces = dot( br_0_transformation_matrix[ :3, :3 ] @ br_0_forces, -direction )
+		all_forces += dot( br_1_transformation_matrix[ :3, :3 ] @ br_1_forces, direction )
+		all_forces += dot( br_0_perturbation[ :3 ], direction )
+		all_forces += dot( br_1_perturbation[ :3 ], -direction )
 
-		return perturbation_01_0, perturbation_01_1
+		perturbation = direction * all_forces
+
+		# in world frame
+		return perturbation, -perturbation
 
 
 if __name__ == "__main__":
@@ -160,44 +169,63 @@ if __name__ == "__main__":
 
 	model = Model( dynamics, time_step, initial_state, initial_actuation )
 
-	_, ((ax1, ax2), (ax3, ax4)) = plt.subplots( 2, 2 )
-
 	x0s = [ ]
 	x1s = [ ]
 
 	ds = [ ]
+	t0s = [ ]
 	t1s = [ ]
-	t2s = [ ]
+	l = 2.5
 
-	model.actuation[ dynamics.br_0_actuation ][ 0 ] = 10.
+	model.actuation[ dynamics.br_0_actuation ][ 0 ] = 2.5
+	model.actuation[ dynamics.br_0_actuation ][ 1 ] = 2.5
+	model.actuation[ dynamics.br_0_actuation ][ 2 ] = 6.5
+
 	for frame in tqdm( range( n_frames ) ):
 		initial_state = deepcopy( model.state )
 		model.step()
 
 		if any( isnan( model.state ) ):
 			print( f'nan at {frame=}' )
-			# model.state = deepcopy(initial_state)
-			# model.step()
 			break
 
-		t1, t2 = dynamics.c_01.get_perturbations(
+		t0, t1 = dynamics.c_01.get_perturbations(
 				model.state[ dynamics.br_0_position ], model.state[ dynamics.br_1_position ]
 				)
-		if t1 is None:
-			t1, t2 = dynamics.get_taunt_cable_perturbations( model.state, model.actuation, 0 )
+		if t0 is None:
+			t0, t1 = dynamics.get_taunt_cable_perturbations( model.state, model.actuation, 0 )
 
 		ds += [ norm( model.state[ dynamics.br_1_position ] - model.state[ dynamics.br_0_position ] ) ]
-		t1s += [ t1[ 0 ] ]
-		t2s += [ t2[ 0 ] ]
+		t0s += [ norm( t0 ) ]
+		t1s += [ norm( t1 ) ]
 
-		x0s += [ model.state[ dynamics.br_0_position ][ 0 ] ]
-		x1s += [ model.state[ dynamics.br_1_position ][ 0 ] ]
+		x0s += [ model.state[ dynamics.br_0_position ] ]
+		x1s += [ model.state[ dynamics.br_1_position ] ]
 
-	ax1.scatter( ds, t1s, c = linspace( 0., 1., len( ds ) ), cmap = 'summer' )
-	ax2.scatter( ds, t2s, c = linspace( 0., 1., len( ds ) ), cmap = 'summer' )
+	x0s, x1s = array( x0s ), array( x1s )
+	T = [ i * time_step for i in range( len( x0s ) ) ]
 
-	ax3.plot( x0s )
-	ax3.plot( x1s )
-	ax4.plot( [ abs( x1 - x0 ) for x0, x1 in zip( x0s, x1s ) ] )
+	_, ((ax1, ax3), (ax2, ax4)) = plt.subplots( 2, 2 )
+
+	ax1.scatter( ds, t0s, c = linspace( 0., 1., len( ds ) ), cmap = 'summer' )
+	ax2.scatter( ds, t1s, c = linspace( 0., 1., len( ds ) ), cmap = 'summer' )
+	ax2.set_xlabel( 'distances entre br_0 et br_1 [m]' )
+	ax1.set_ylabel( 'force du câble sur br_0 [N]' )
+	ax2.set_ylabel( 'force du câble sur br_1 [N]' )
+
+	ax3.plot( T, x0s[:, 0 ], color = 'r' )
+	ax3.plot( T, x1s[:, 0 ], color = 'b' )
+	ax3.plot( T, x0s[:, 1 ], color = 'r' )
+	ax3.plot( T, x1s[:, 1 ], color = 'b' )
+	ax3.plot( T, x0s[:, 2 ], color = 'r' )
+	ax3.plot( T, x1s[:, 2 ], color = 'b' )
+	ax3.set_xlabel( 'time [s]' )
+	ax3.set_ylabel( 'position sur $x_w$' )
+	ax3.legend( [ 'br_0', 'br_1' ] )
+
+	ax4.plot(T, [ norm( x1 - x0 ) for x0, x1 in zip( x0s, x1s ) ] )
+	ax4.axhline( 3. )
+	ax4.set_xlabel( 'time [s]' )
+	ax4.set_ylabel( 'distance entre br_0 et br_1' )
 
 	plt.show()
